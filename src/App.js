98c3899f5a3e1f09050051e779f2dbc30b6a88d2
Efren0406 <efrenchito477@gmail.com>
2023-12-09 21:08:17 -0600

    Data taken from json

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import cloudy from './assets/weather-3440560.svg'
 import partialyCloud from './assets/weather-3440563.svg'
 import windy from './assets/weather-3443044.svg'
 import './App.css';
+import API from './API/weather.json'
+
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [weatherData, setWeatherData] = useState([]);
+  const date = new Date();
+  const city = 1;
+  const weekDays = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+
+  useEffect(() => {
+    setWeatherData(API);
+  }, []);
+
   return (
     <div className="cityView">
       <div>
@@ -25,15 +37,29 @@ function App() {
             </h1>
           </div>
 
+          <div className='cityName'>
+            <p>
+              {weatherData[city] ? weatherData[city].nmun : 'Cargando...'}
+            </p>
+          </div>
+
+
           <div className='line'>
             <hr />
           </div>
 
           <div className='todayInfo'>
             <p>
-              12:24:10 pm <br />
-              05 de diciembre <br />
-              2023 <br />
+              {weekDays[(date.getDay()) % 7]} <br />
+              {date.getDate()} {date.getMonth() + 1} {date.getFullYear()}<br />
+            </p>
+          </div>
+
+          <div className='weather'>
+            <p>
+              {weatherData[city] ? weatherData[city].tmin : 'Cargando...'}° / {weatherData[city] ? weatherData[city].tmax : 'Cargando...'}° <br />
+              {weatherData[city] ? weatherData[city].probprec : 'Cargando...'} % <br />
+              {weatherData[city] ? weatherData[city].desciel : 'Cargando...'}
             </p>
           </div>
 
@@ -50,27 +76,28 @@ function App() {
 
       <div className='otherDays'>
         <div className='dayInfo' id='second'>
-          <p>Sábado</p>
-          <p>12° / 20°</p>
-          <img src={rainIcon}></img>
+          <p>
+            {weekDays[(new Date().getDay() + 1) % 7]} <br />
+            {weatherData[city + 1] ? weatherData[city + 1].tmin : 'Cargando...'}° / {weatherData[city + 1] ? weatherData[city + 1].tmax : 'Cargando...'}° <br />
+          </p>
+          <img src={rainIcon} alt='Icono de lluvia'></img>
         </div>
 
+
         <div className='dayInfo' id='third'>
-          <p>Domingo</p>
-          <p>8° / 30°</p>
+          <p>
+            {weekDays[(new Date().getDay() + 2) % 7]} <br />
+            {weatherData[city + 2] ? weatherData[city + 2].tmin : 'Cargando...'}° / {weatherData[city + 2] ? weatherData[city + 2].tmax : 'Cargando...'}° <br />
+          </p>
           <img src={cloudy}></img>
         </div>
 
         <div className='dayInfo' id='fourth'>
-          <p>Lunes</p>
-          <p>9° / 28°</p>
+          <p>
+            {weekDays[(new Date().getDay() + 3) % 7]} <br />
+            {weatherData[city + 3] ? weatherData[city + 3].tmin : 'Cargando...'}° / {weatherData[city + 3] ? weatherData[city + 3].tmax : 'Cargando...'}° <br />
+          </p>
           <img src={partialyCloud}></img>
-        </div>
-
-        <div className='dayInfo' id='fifth'>
-          <p>Martes</p>
-          <p>10° / 31°</p>
-          <img src={windy}></img>
         </div>
       </div>
     </div>
